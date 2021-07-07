14f120bd57183b79831e53da3d338b288e816286
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-07-07 16:04:53 +0500

    Update index.js

--- ...../index.js
+++ ...../index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const getToken = require('./token');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.static('public'));
