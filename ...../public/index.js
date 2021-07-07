093b6db325b60e053922eda6b1995673116d7bd1
rashidali711 <83244505+rashidali711@users.noreply.github.com>
2021-07-07 21:40:03 +0500

    Update index.js

--- ...../public/index.js
+++ ...../public/index.js
@@ -1,7 +1,7 @@
 $('#getToken').click(() => {
   let id = $('#email').val();
   let pw = $('#password').val();
-  fetch(`http://localhost:8080/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
+  fetch(`http://localhost:11501/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
     .then(e => e.json())
     .then(e => {
       let res = e.loc || e.error;
